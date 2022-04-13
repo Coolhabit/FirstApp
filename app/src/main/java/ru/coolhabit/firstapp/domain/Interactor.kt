c7e515e60700165feb32e554cd45e9b058a52b7d
Coolhabit <coolhabit92@gmail.com>
2022-04-13 23:57:13 +0300

    45 - rxjava search

--- app/src/main/java/ru/coolhabit/firstapp/domain/Interactor.kt
+++ app/src/main/java/ru/coolhabit/firstapp/domain/Interactor.kt
@@ -59,5 +59,10 @@ class Interactor(private val repo: MainRepository, private val retrofitService: 
     fun getDefaultCategoryFromPreferences() = preferences.getDefaultCategory()
 
     fun getFilmsFromDB(): Observable<List<Film>> = repo.getAllFromDB()
+
+    fun getSearchResultFromApi(search: String): Observable<List<Film>> = retrofitService.getFilmFromSearch(API.KEY, "ru-RU", search, 1)
+        .map {
+            Converter.convertApiListToDtoList(it.tmdbFilms)
+        }
 }
 
--- app/src/main/java/ru/coolhabit/firstapp/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/ru/coolhabit/firstapp/viewmodel/HomeFragmentViewModel.kt
@@ -34,4 +34,7 @@ class HomeFragmentViewModel : ViewModel() {
     fun getFilms() {
         interactor.getFilmsFromApi(1)
     }
+
+    fun getSearchResult(search: String) = interactor.getSearchResultFromApi(search)
+
 }
