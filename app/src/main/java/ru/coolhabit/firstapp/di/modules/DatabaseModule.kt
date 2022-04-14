dd1e205419252a750a499aeb7a788fa57591bcc0
Coolhabit <coolhabit92@gmail.com>
2022-04-14 14:42:03 +0300

    45 - rxjava search + migration

--- app/src/main/java/ru/coolhabit/firstapp/di/modules/DatabaseModule.kt
+++ app/src/main/java/ru/coolhabit/firstapp/di/modules/DatabaseModule.kt
@@ -2,6 +2,8 @@ package ru.coolhabit.firstapp.di.modules
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import dagger.Module
 import dagger.Provides
 import ru.coolhabit.firstapp.data.MainRepository
@@ -18,10 +20,18 @@ class DatabaseModule {
             context,
             AppDatabase::class.java,
             "film_db"
-        ).fallbackToDestructiveMigration()
+        ).addMigrations(MIGRATION_1_2)
             .build().filmDao()
 
     @Provides
     @Singleton
     fun provideRepository(filmDao: FilmDao) = MainRepository(filmDao)
+
+    companion object {
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE cached_films ALTER COLUMN poster_path STRING")
+            }
+        }
+    }
 }
