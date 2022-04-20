411541d4c51f971bd13f599bb630ff6a0c9d468d
Coolhabit <coolhabit92@gmail.com>
2022-04-20 12:54:00 +0300

    47 - broadcastreceiver

--- app/src/main/java/ru/coolhabit/firstapp/view/MainActivity.kt
+++ app/src/main/java/ru/coolhabit/firstapp/view/MainActivity.kt
@@ -1,5 +1,8 @@
 package ru.coolhabit.firstapp.view
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -7,6 +10,7 @@ import androidx.fragment.app.Fragment
 import ru.coolhabit.firstapp.*
 import ru.coolhabit.firstapp.databinding.ActivityMainBinding
 import ru.coolhabit.firstapp.data.entity.Film
+import ru.coolhabit.firstapp.utils.ConnectionChecker
 import ru.coolhabit.firstapp.view.fragments.*
 
 
@@ -17,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +39,14 @@ class MainActivity : AppCompatActivity() {
             .add(R.id.fragment_placeholder, HomeFragment())
             .addToBackStack(null)
             .commit()
+
+        receiver = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filters)
+
     }
 
     fun launchDetailsFragment(film: Film) {
@@ -107,5 +121,8 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
 }
